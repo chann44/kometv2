f7ec90d6035d323f7db2ef53b92db30433953018
= <=>
2022-06-08 17:55:32 +0530

    team page data added

--- src/Componets/Aboutus/TeamCard.tsx
+++ src/Componets/Aboutus/TeamCard.tsx
@@ -1,27 +1,34 @@
 import { AiFillGithub, AiFillTwitterCircle } from "react-icons/ai";
 
-const TeamCard = () => {
+
+interface Props {
+    name: string,
+    img: string,
+    twitter: string
+    role: string
+}
+
+const TeamCard = ({ name, img, twitter, role }: Props) => {
     return (
         <>
-
-            <div className=" flex my-1 flex-col items-center px-4 py-5 space-y-4  lg:w-[24%] md:w-[50%] w-full rounded-xl">
+            <div className=" flex my-1 flex-col items-center px-4 py-5 space-y-4  lg:w-[24%] md:w-[33%] w-[50%] rounded-xl">
                 <div className="w-20 rounded-full ">
-                    <img className="object-cover rounded-full" src="https://pps.whatsapp.net/v/t61.24694-24/177259321_223651216311553_3724816846215524529_n.jpg?ccb=11-4&oh=039d7534dd21221659f54a6f1f86759b&oe=62A3EB86" alt="" />
+                    <img className="object-cover rounded-full" src={img} alt="" />
                 </div>
                 <div className="text-center space-y-4">
                     <div className="">
-                        <h1 className="text-sm">Kush Ratan Gupta</h1>
-                        <p className="text-xs">CEO & Founder</p>
+                        <h1 className="text-lg font-bold">{name}</h1>
+                        <p className="text-xs">{role}</p>
                     </div>
-                    <p className="text-xs">Curious about tools, defi and tech dogs </p>
+                    {/* <p className="text-xs">Curious about tools, defi and tech dogs </p> */}
 
                 </div>
                 <div className="flex space-x-4 text-3xl">
-                    <AiFillGithub />
-                    <AiFillTwitterCircle />
+                    <a href={twitter} target="_blank">
+                        <AiFillTwitterCircle />
+                    </a>
                 </div>
             </div>
-
         </>
     )
 }
--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -5,6 +5,89 @@ import JoinWaitlist from "../Componets/Join waitlist/index";
 import Navbar from "../Componets/Navbar";
 
 
+const TeamData = [
+    {
+        name: "RatnaKush",
+        imglink: "https://pbs.twimg.com/profile_images/1242143465875505153/cvwgn6GA_400x400.jpg",
+        twitter: "https://twitter.com/RatnaKush",
+        role: "CEO & Founder"
+    },
+    {
+        name: "Vedant",
+        imglink: "https://pbs.twimg.com/profile_images/1516143426928267264/2KsuHusi_400x400.jpg",
+        twitter: "https://twitter.com/_vedantpawar_",
+        role: "CTO & Founder"
+    },
+    {
+        name: "satyam",
+        imglink: "https://pbs.twimg.com/profile_images/1508937780084318214/pcjY-hFq_400x400.jpg",
+        twitter: "https://twitter.com/sk1122_",
+        role: "backend & solidity Enigneer"
+    },
+    {
+        name: "Shivam",
+        imglink: "https://pbs.twimg.com/profile_images/1311286667685187584/gQ1S_TAR_400x400.jpg",
+        twitter: "https://twitter.com/shivaam23",
+        role: "Solidity enginner"
+    },
+    {
+        name: "Vikash",
+        imglink: "https://pbs.twimg.com/profile_images/1495293615970873348/eI7Zl7pC_400x400.jpg",
+        twitter: "https://twitter.com/44_chann",
+        role: "frontend developer "
+    },
+    {
+        name: "Anmol",
+        imglink: "https://pbs.twimg.com/profile_images/1518993545574502400/q2dvX3bS_400x400.jpg",
+        twitter: "https://twitter.com/anmol_gh_",
+        role: "community manager"
+    },
+    {
+        name: "Ishita Pandey",
+        imglink: "https://pbs.twimg.com/profile_images/1427140773464608775/stWNiQei_400x400.jpg",
+        twitter: "https://twitter.com/ishitaapandey?s=21&t=HJLClsV7its6YU4YaddL-Q",
+        role: "social media manager"
+    },
+    {
+        name: "Pooja Yamsalwar",
+        imglink: "https://pbs.twimg.com/profile_images/1534160248772390913/1BmWM-WM_400x400.jpg",
+        twitter: "https://twitter.com/poojaaaa05?https://twitter.com/poojaaaa05?t=T2Waxpfu54amK2m9fF",
+        role: "ui/ux designer"
+    },
+    {
+        name: "Mahaparsad",
+        imglink: "https://pbs.twimg.com/profile_images/1478382095999377410/1gUnt9P2_400x400.jpg",
+        twitter: "https://twitter.com/mahaprasad_",
+        role: "socail media manager"
+    },
+    {
+        name: "Aditiya Raj",
+        imglink: "https://pbs.twimg.com/profile_images/1506490684085514240/IbkaaHwi_400x400.jpg",
+        twitter: "https://twitter.com/arktwts",
+        role: "backend enginner"
+    },
+    {
+        name: "Aditiya Gupta",
+        imglink: "https://pbs.twimg.com/profile_images/1488089831028658178/sYtlUjXp_400x400.jpg",
+        twitter: "https://twitter.com/adiig7",
+        role: "devloper community manager"
+    },
+    {
+        name: "Sabsatine",
+        imglink: "https://pbs.twimg.com/profile_images/1526314137843793923/4LUwR2E5_400x400.jpg",
+        twitter: "https://twitter.com/Sabastine_Iroka?t=Wnpod_-AEiMrStgkuO4cxA&s=09",
+        role: "content creator"
+    },
+    {
+        name: "Shubham Upadhayay",
+        imglink: "https://pbs.twimg.com/profile_images/1521876011138224128/WFaNNySB_400x400.jpg",
+        twitter: "https://twitter.com/iamshubhamu?t=ZppUVAdCg4xZLlw-vtMf7Q&s=08",
+        role: "devloper relations"
+
+    }
+]
+
+
 const Aboutus = () => {
     return (
         <><div className="text-opacity-90">
@@ -18,14 +101,11 @@ const Aboutus = () => {
                 <h1 className="text-4xl lg:text-5xl text-center font-bold">Our <span className="counter">Team</span></h1>
                 <p className="text-center text-lg py-4">We have small but growing remote first team</p>
                 <div className="min-h-screen my-8 w-full  lg:w-[80%] flex content-start justify-center flex-wrap text-white  mx-auto">
-                    <TeamCard />
-                    <TeamCard />
-                    <TeamCard />
-                    <TeamCard />
-                    <TeamCard />
-                    <TeamCard />
-                    <TeamCard />
-                    <TeamCard />
+                    {
+                        TeamData && TeamData.map((item) => {
+                            return <TeamCard name={item.name} twitter={item.twitter} img={item.imglink} role={item.role} />
+                        })
+                    }
                 </div>
 
                 <div className="min-h-screen w-[80%] flex flex-col justify-center m-auto">
